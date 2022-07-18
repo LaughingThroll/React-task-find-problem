f7b919b1c032cedea34a8cf9a991be35a021ff28
LaughingThroll <LaughingThroll@gmail.com>
2022-07-18 15:42:38 +0300

    add task

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import React, { useState, useEffect } from 'react'
+
+// имитация запроса к серверу. просто получаем число асинхронно
+const fetchRandomNumber = () => Promise.resolve(Math.random())
+
 export const App = () => {
-  return <div>Hello</div>
+  const [number, setNumber] = useState()
+  const [scroll, setScroll] = useState()
+
+  useEffect(async () => {
+    setNumber(await fetchRandomNumber())
+
+    window.addEventListener('scroll', () => setScroll(window.scrollY))
+
+    return () =>
+      window.removeEventListener('scroll', () => setScroll(window.scrollY))
+  })
+
+  return (
+    <div>
+      <div> Number: {number} </div>
+      <div> Scroll: {scroll} </div>
+    </div>
+  )
 }
